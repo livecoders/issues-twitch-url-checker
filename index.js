0d394772c176218be793cc82da5fc2daddc42175
Andy Morrell <andy@andymorrell.net>
2020-05-07 15:58:54 +0100

    added more

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 const clientId = 'lrmknreis8iakk53pwt87469523kr6';
+const NO_TWITCH_URL_FOUND = "We couldn't find a twitch URL in your application"
 const NOT_AFFILIATE_OR_PARTNER = "It looks like you're not Twitch affiliate or partner yet. Please reapply when you've met all the requirements. We'd be happy to evaluate your application then."
 
 async function addLabel(client, labelName) {
@@ -60,29 +61,41 @@ async function run() {
       if (github.context.payload.issue.labels.filter(label => label.name === "application").length > 0) {
         console.log('Has application label');
         let urls = github.context.payload.issue.body.match(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm);
-        console.log(`Has ${urls.length} urls in the body`);
-        for (let index in urls) {
-          console.log(urls[index]);
-          let url = new URL(urls[index]);
-          if (url.host.includes('twitch.tv') && (url.pathname.match(/\//g) || []).length == 1) {
-            let login = url.pathname.replace('/', '').toLowerCase();
-            //get user from twitch
-            var appaccessToken = await getAppAccessToken();
-            let result = await axios.get(`https://api.twitch.tv/helix/users?login=${login}`, { headers: { 'Client-Id': clientId, 'Authorization': `Bearer ${appaccessToken}` } });
-            
-            if (result.data.data.length === 1) {
-              let user = result.data.data[0];
-              const token = core.getInput('repo-token', {required: true});
-              const client = new github.GitHub(token);
 
-              if (user.broadcaster_type === 'affiliate' || user.broadcaster_type === 'partner') {
-                await addLabel(client, user.broadcaster_type);
-              } else {
-                console.log("User is not affiliate or partner yet. Commenting and closing issue.");
-                await addComment(client, NOT_AFFILIATE_OR_PARTNER);
-                await closeIssue(client);
+        if(urls == null || urls.length == 0) {
+          await addComment(client, NO_TWITCH_URL_FOUND);
+        } else {
+          console.log(`Has ${urls.length} urls in the body`);
+          let found = false;
+          for (let index in urls) {
+            console.log(urls[index]);
+            let url = new URL(urls[index]);
+            if (url.host.includes('twitch.tv') && (url.pathname.match(/\//g) || []).length == 1) {
+              console.log('Twitch Url Found');
+              found = true;
+              let login = url.pathname.replace('/', '').toLowerCase();
+              //get user from twitch
+              var appaccessToken = await getAppAccessToken();
+              let result = await axios.get(`https://api.twitch.tv/helix/users?login=${login}`, { headers: { 'Client-Id': clientId, 'Authorization': `Bearer ${appaccessToken}` } });
+              
+              if (result.data.data.length === 1) {
+                let user = result.data.data[0];
+                const token = core.getInput('repo-token', {required: true});
+                const client = new github.GitHub(token);
+
+                if (user.broadcaster_type === 'affiliate' || user.broadcaster_type === 'partner') {
+                  await addLabel(client, user.broadcaster_type);
+                } else {
+                  console.log("User is not affiliate or partner yet. Commenting and closing issue.");
+                  await addComment(client, NOT_AFFILIATE_OR_PARTNER);
+                  await closeIssue(client);
+                }
               }
             }
+          }
+
+          if (!found) {
+            await addComment(client, NO_TWITCH_URL_FOUND);
           }
         }
       }
